7d8ebb0d382835ba05137a46e42c54fd58b7b494
Hannah Mcevoy <lukiangel57@gmail.com>
2023-11-14 15:08:17 -0500

    ABle to complete code, needs debugging

---
+++ README.md
@@ -0,0 +1,56 @@
+# SVG Logo Maker
+
+## Description
+The SVG Logo Maker is a Node.js command-line application that enables users to generate custom SVG logos. It provides an interactive prompt, allowing users to choose text, text color, shape, and shape color to create a personalized logo. The application saves the generated SVG file for easy use in web development projects.
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [License](#license)
+- [Walkthrough Video](#walkthrough-video)
+- [Questions](#questions)
+
+## Installation
+1. Clone the repository to your local machine:
+
+    ```bash
+    git clone https://github.com/your-username/SVG_Logo_Maker.git
+    ```
+
+2. Navigate to the project directory:
+
+    ```bash
+    cd SVG_Logo_Maker
+    ```
+
+3. Install the required dependencies:
+
+    ```bash
+    npm install
+    ```
+
+## Usage
+1. Run the application using the following command:
+
+    ```bash
+    node index.js
+    ```
+
+2. Answer the prompted questions to customize your SVG logo, including text, text color, shape, and shape color.
+
+3. The generated SVG file, named `logo.svg`, will be saved in the project directory.
+
+4. Open the `logo.svg` file in a browser to view the generated logo.
+
+![SVG Logo Maker Demo](./assets/demo.gif)
+
+## Contributing
+Contributions to this project are currently not accepted.
+
+## Tests
+To run unit tests, use the following command:
+
+```bash
+npm test
---
+++ index.js
@@ -0,0 +1,69 @@
+import fs from 'fs/promises';
+import { Circle, Square, Triangle } from './lib/shapes.js';
+import inquirer from 'inquirer';
+
+// Function to generate a logo SVG with specified text, text color, shape, and shape color.
+function generateLogo(text, textColor, shape, shapeColor) {
+    if (!validateColor(textColor) || !validateColor(shapeColor)) {
+        console.log('Invalid color format');
+        return;
+    }
+
+    const shapeSVG = shape.getSVG(getSVG(shapeColor));
+
+    const logoSVG = `
+  <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+    ${shapeSVG}
+    <text x="150" y="130" fill="${convertToHex(textColor)}" font-size="36" text-anchor="middle">${text}</text>
+  </svg>
+`;
+
+    fs.writeFileSync('logo.svg', logoSVG);
+    console.log('Generated logo.svg');
+}
+
+// Use Inquirer to prompt the user for input.
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'text',
+            message: 'Enter up to three characters:',
+        },
+        {
+            type: 'input',
+            name: 'textColor',
+            message: 'Enter text color (keyword or hex):',
+        },
+        {
+            type: 'list',
+            name: 'shapeChoice',
+            message: 'Choose a shape:',
+            choices: ['circle', 'triangle', 'square'],
+        },
+        {
+            type: 'input',
+            name: 'shapeColor',
+            message: 'Enter shape color (keyword or hex):',
+        },
+    ])
+    // Once the user provides answers to prompts...
+    .then(answers => {
+        const { text, textColor, shapeChoice, shapeColor } = answers;
+        const ShapeClass = {
+            circle: Circle,
+            triangle: Triangle,
+            square: Square
+        }[shapeChoice.toLowerCase()]; // Get the appropriate shape class based on the user's choice.
+
+        if (ShapeClass) {
+            const shapeInstance = new ShapeClass(); // Create an instance of the selected shape class.
+            generateLogo(text, textColor, shapeInstance, shapeColor); // Generate the logo using the specified inputs.
+        } else {
+            console.log('Invalid shape choice');
+        }
+    })
+    .catch(error => {
+        console.error('An error occurred:', error);
+
+    });
